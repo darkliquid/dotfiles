c84255a23d0be918e54617fa4057cdb3d7380aa7
Andrew Montgomery <darkliquid@darkliquid.co.uk>
2019-04-13 08:32:36 +0100

    Add bash file to setup wsl-open BROWSER vars
    Add scripts to ensure wsl-open can handle URLs

---
+++ bash/.bash.d/wsl-open.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Adding wsl-open as a browser for Bash for Windows
+if [[ $(uname -r) == *Microsoft ]]; then
+	if [[ -z $BROWSER ]]; then
+		export BROWSER=wsl-open
+	else
+		export BROWSER=$BROWSER:wsl-open
+	fi
+fi
