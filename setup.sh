3a6d76f4547e856ce9411dfaed811e8294ed0a9e
Andrew Montgomery <darkliquid@darkliquid.co.uk>
2022-01-06 13:56:14 +0000

    switch to relying on npiperelay from wsl-ssh-agent for ssh-agent bridging and prefer ssh for github access

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ function msg() {
 
 # Basic install requirements (check if installed first so we can skip sudo)
 msg "Installing essentials..."
-pkgs=( build-essential curl file git stow xdg-utils libsecret-1-0 libsecret-1-dev libglib2.0-dev )
+pkgs=( socat build-essential curl file git stow xdg-utils libsecret-1-0 libsecret-1-dev libglib2.0-dev )
 for pkg in "${pkgs[@]}"; do
 	dpkg-query --show --showformat='${db:Status-Status}' $pkg &> /dev/null
 	if [ $? -ne 0 ]; then
