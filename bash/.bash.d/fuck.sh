d57b95a0df31faacd840a20e58155a83905ccfdc
Andrew Montgomery <andrewm@streamelements.com>
2022-01-10 14:59:40 +0000

    tweak thefuck config for wsl, disable instant mode

--- bash/.bash.d/fuck.sh
+++ bash/.bash.d/fuck.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 if [ -x "$(command -v thefuck)" ]; then
-  eval $(thefuck --alias ffs --enable-experimental-instant-mode)
+  eval $(thefuck --alias ffs)
+
+  # Thefuck is incredibly slow on WSL if we let it traverse into Windows
+  # so we set this config option to prevent it.
+  if [[ -n "$IS_WSL" || -n "$WSL_DISTRO_NAME" ]]; then
+    mkdir -p ~/.config/thefuck
+    if ! grep -q "^excluded_search_path_prefixes" ~/.config/thefuck/settings.py; then
+      echo "excluded_search_path_prefixes = ['/mnt/']" >> ~/.config/thefuck/settings.py
+    fi
+  fi
 fi
