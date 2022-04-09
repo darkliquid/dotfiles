77f698b5ba123168e3a55d2df02881676ed5b688
Andrew Montgomery <andrewm@streamelements.com>
2022-04-09 11:47:46 +0100

    add yarn

--- bash/.bash.d/node.sh
+++ bash/.bash.d/node.sh
@@ -3,3 +3,5 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm"
+export YARN_DIR="$HOME/.yarn"
+export PATH="$PATH:$YARN_DIR/bin"
