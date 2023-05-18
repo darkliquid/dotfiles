8fff803da35ab360db440ec575ca7f12b1a2d8d1
Andrew Montgomery <andrewm@streamelements.com>
2023-05-18 09:14:19 +0100

    more qs to close

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -57,10 +57,12 @@ vim.api.nvim_create_autocmd("FileType", {
     "toggleterm",
     "tsplayground",
     "vim",
+    "TelescopePrompt"
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+    vim.keymap.set("n", "q", "<cmd>close!<cr>", { buffer = event.buf, silent = true })
+    vim.keymap.set("n", "<leader>q", "<cmd>close!<cr>", { buffer = event.buf, silent = true })
   end,
 })
 
