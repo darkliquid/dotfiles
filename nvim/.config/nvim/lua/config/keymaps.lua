3d83f2994b140b177d34caa2dace3184916bb2da
Andrew Montgomery <andrewm@streamelements.com>
2023-06-09 12:51:00 +0100

    nvim tweaks

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,4 +3,4 @@
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>?", "<cmd>WhichKey<cr>", { desc = "Show keymap help" })
 vim.keymap.set("n", "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", { desc = "Toggle current line blame" })
-vim.keymap.set("v", "<leader>gl", "<cmd>GetCurrentBranchLink<cr>", { desc = "Get link to code on github" })
+vim.keymap.set("v", "<leader>gl", "<cmd>'<,'>GetCurrentBranchLink<cr>", { desc = "Get link to code on github" })
--- nvim/.config/nvim/lua/plugins/overrides.lua
+++ nvim/.config/nvim/lua/plugins/overrides.lua
@@ -9,6 +9,48 @@ return {
     "lvimuser/lsp-inlayhints.nvim",
     branch = "anticonceal",
   },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-cmdline",
+    },
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      -- `:` cmdline setup.
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline({
+          ["<Down>"] = {
+            c = function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              else
+                fallback()
+              end
+            end,
+          },
+          ["<Up>"] = {
+            c = function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              else
+                fallback()
+              end
+            end,
+          },
+        }),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          {
+            name = "cmdline",
+            option = {
+              ignore_cmds = { "Man", "!" },
+            },
+          },
+        }),
+      })
+    end,
+  },
   -- add github_dark theme
   {
     "projekt0n/github-nvim-theme",
