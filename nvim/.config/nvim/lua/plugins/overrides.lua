107400b2ed141216c748b8931f086096df69bbff
Andrew Montgomery <andrewm@streamelements.com>
2023-07-10 12:20:56 +0100

    feat: add snippet search

--- nvim/.config/nvim/lua/plugins/overrides.lua
+++ nvim/.config/nvim/lua/plugins/overrides.lua
@@ -14,12 +14,52 @@ return {
     },
   },
   {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+  {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "hrsh7th/cmp-cmdline",
     },
     opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local luasnip = require("luasnip")
       local cmp = require("cmp")
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+            cmp.select_next_item()
+          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+          -- this way you will only jump inside the snippet region
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+
       -- `:` cmdline setup.
       cmp.setup.cmdline(":", {
         mapping = cmp.mapping.preset.cmdline({
@@ -112,11 +152,22 @@ return {
   -- add telescope-fzf-native
   {
     "telescope.nvim",
+    keys = {
+      {
+        "<leader>sl",
+        function()
+          require("telescope").extensions.luasnip.luasnip({})
+        end,
+        desc = "Snippets",
+      },
+    },
     dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
+      "benfowler/telescope-luasnip.nvim",
       build = "make",
       config = function()
         require("telescope").load_extension("fzf")
+        require("telescope").load_extension("luasnip")
       end,
       opts = function(_, opts)
         local telescopeConfig = require("telescope.config")
