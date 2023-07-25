e9757174482cfedc4040976b914ecbfab8d3c1dc
Andrew Montgomery <andrewm@streamelements.com>
2023-07-25 10:10:56 +0100

    nvim tweaks

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -3,5 +3,10 @@ return {
     "pocco81/auto-save.nvim",
     config = true,
     event = "BufEnter",
-  }
+  },
+  {
+    "chrisgrieser/nvim-early-retirement",
+    config = true,
+    event = "VeryLazy",
+  },
 }
