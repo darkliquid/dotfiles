2bfb09aa7ec7dcd0efaa9984b0454324f9ad512a
Andrew Montgomery <andrewm@streamelements.com>
2023-07-26 16:12:38 +0100

    nvim tweaks

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,4 @@
 vim.keymap.set("n", "<leader>?", "<cmd>WhichKey<cr>", { desc = "Show keymap help" })
 vim.keymap.set("n", "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", { desc = "Toggle current line blame" })
 vim.keymap.set("v", "<leader>gl", "<cmd>'<,'>GetCurrentBranchLink<cr>", { desc = "Get link to code on github" })
+vim.keymap.set("n", "<leader>ut", "<cmd>Neotree<cr>", { desc = "Jump to tree" })
