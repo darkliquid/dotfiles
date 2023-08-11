6b98656128a3ce22b2b98d9eceadced43e353e81
Andrew Montgomery <darkliquid@darkliquid.co.uk>
2023-08-11 16:13:33 +0100

    fix: ble.sh integration

--- bash/.bash.d/ble.sh
+++ bash/.bash.d/ble.sh
@@ -3,6 +3,7 @@
 BLESH_SRC=~/.local/share/blesh/ble.sh
 
 if [ ! -f "$BLESH_SRC" ]; then
+  mkdir -p "$(dirname $BLESH_SRC)"
   curl -sL https://api.github.com/repos/akinomyoga/ble.sh/releases/latest | jq -r '.assets[0].browser_download_url' | xargs curl -qL | tar xJf - -C ~/.local/share/blesh --strip-components=1
   chmod +x $BLESH_SRC
 fi
