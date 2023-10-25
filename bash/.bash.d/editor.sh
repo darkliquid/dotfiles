eaf284166e6b9642e7db4ce9f47529c5e2dc30d7
Andrew Montgomery <andrewm@streamelements.com>
2023-10-25 10:36:24 +0100

    nvim updates

--- bash/.bash.d/editor.sh
+++ bash/.bash.d/editor.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 export EDITOR=nvim
+
+update_nvim ()
+{
+  gum spin --title="Updating nvim..." -- bob update --all
+  gum spin --title="Updating nvim plugins..." -- nvim --headless "+Lazy! update" +qa
+}
--- config/.config/nvim/lua/plugins/overrides.lua
+++ config/.config/nvim/lua/plugins/overrides.lua
@@ -194,14 +194,6 @@ return {
     opts = { use_diagnostic_signs = true },
   },
 
-  -- add symbols-outline
-  {
-    "simrat39/symbols-outline.nvim",
-    cmd = "SymbolsOutline",
-    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-    config = true,
-  },
-
   -- add telescope-fzf-native
   {
     "telescope.nvim",
