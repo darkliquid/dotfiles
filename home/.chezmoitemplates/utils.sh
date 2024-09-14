050d1c5933068110bf81d859500ee10096849a20
Andrew Montgomery <darkliquid@darkliquid.co.uk>
2024-09-14 23:30:46 +0100

    refactor: reorganise in prep for windows

---
+++ home/.chezmoitemplates/utils.sh
@@ -0,0 +1,30 @@
+# exist checks if a command exist in shell
+exist() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# log writes message to stdout with a timestamp in blue
+log() {
+    printf "\033[33;34m [%s] %s\n" "$(date)" "$1"
+}
+
+# Append entry to PATH without duplicating existing entries
+pathappend() {
+  for ARG in "$@"
+  do
+    if [ -d "$ARG" ] && [[ ":$PATH:" != *":$ARG:"* ]]; then
+        PATH="${PATH:+"$PATH:"}$ARG"
+    fi
+  done
+}
+
+# Prepend entry to PATH without duplicating existing entries
+pathprepend() {
+  for ((i=$#; i>0; i--)); 
+  do
+    ARG=${!i}
+    if [ -d "$ARG" ] && [[ ":$PATH:" != *":$ARG:"* ]]; then
+        PATH="$ARG${PATH:+":$PATH"}"
+    fi
+  done
+}
