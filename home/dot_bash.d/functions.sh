2bfdaed56a49d7aa111f45b100d4c8435686d514
Andrew Montgomery <darkliquid@darkliquid.co.uk>
2025-02-16 10:04:12 +0000

    feat(mise): tweak mise config/pkgs

---
+++ home/dot_bash.d/functions.sh
@@ -0,0 +1,20 @@
+# Load SSH identities if required.
+function load_ssh_identities() {
+    if [ "$(ssh-add -l)" == "The agent has no identities." ]; then
+        ssh-add
+    fi
+}
+
+# Simple wrapper to run all the updates
+function update() {
+    load_ssh_identities
+    chezmoi update
+    if [ $(id -u) -eq 0 ]; then
+        aptitude update
+        aptitude safe-upgrade
+    else
+        log "Not running as root, skipping apt..."
+    fi
+    brew upgrade
+    mise upgrade
+}
