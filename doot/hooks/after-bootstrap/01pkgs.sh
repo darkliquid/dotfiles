920a92b9483596625b6ce885494dd39c98a341a5
Andrew Montgomery <darkliquid@darkliquid.co.uk>
2026-04-05 11:09:34 +0100

    pkg fix first so dependencies exist for pkgbuilds

--- doot/hooks/after-bootstrap/01pkgs.sh
+++ doot/hooks/after-bootstrap/01pkgs.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-# Install our local repo packages
-for pkgdir in pkgbuilds/*/; do
-    # Skip dirs lacking a PKGBUILD
-    [ -f "$pkgdir/PKGBUILD" ] || continue
-    (
-        cd "$pkgdir"
-        pkgctl build -i auto
-    )
-done
-
 # Ensure our package state is how we like it
 # after bootstrapping.
 mise x -- pkgstate fix
+
+# Install our local repo packages
+for pkgdir in pkgbuilds/*/; do
+	# Skip dirs lacking a PKGBUILD
+	[ -f "$pkgdir/PKGBUILD" ] || continue
+	(
+		cd "$pkgdir"
+		pkgctl build -i auto
+	)
+done
