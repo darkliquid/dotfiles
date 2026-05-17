61ddf7ef9ddf8233d5409c7cafae93d6505a09c1
Andrew Montgomery <darkliquid@darkliquid.co.uk>
2026-05-17 06:04:36 +0100

    fix fish config

--- config/fish/conf.d/git.fish
+++ config/fish/conf.d/git.fish
@@ -1,1 +1,6 @@
-set -gx GITHUB_TOKEN "$(gh auth token)"
+if command -sq gh
+    set -l token (gh auth token 2>/dev/null)
+    if test $status -eq 0; and test -n "$token"
+        set -gx GITHUB_TOKEN "$token"
+    end
+end
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,4 +3,3 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 # Disable greeting on every shell, it's mostly annoying
 function fish_greeting
 end
-source ~/.config/fish/conf.d/*
