6d932574797061501bcf2f7da34aa2350903929b
Andrew Montgomery <darkliquid@darkliquid.co.uk>
2026-06-18 23:18:25 +0100

    minor fixes/updates

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,3 +3,7 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 # Disable greeting on every shell, it's mostly annoying
 function fish_greeting
 end
+
+
+# Added by Antigravity CLI installer
+set -gx PATH "/home/darkliquid/.local/bin" $PATH
